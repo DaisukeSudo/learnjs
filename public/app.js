2b392a88b13b5a0b5976a727674f0026ddca891e
dsudo <dsudo0624@gmail.com>
2019-10-14 20:20:16 +0900

    Add templates

--- public/app.js
+++ public/app.js
@@ -1,9 +1,30 @@
 'use strict';
 const learnjs = {};
 
-learnjs.problemView = (problemNumber) => {
-  const title = 'Problem #' + problemNumber + ' Coming soon!';
-  return $('<dic class="problem-view">').text(title);
+learnjs.problems = [
+  {
+    description: 'What is truth?',
+    code: 'function problem() { return __; }',
+  },
+  {
+    description: 'Simple Math',
+    code: 'function problem() { return 42 === 6 * __; }',
+  },
+];
+
+learnjs.applyObject = (obj, elem) => {
+  for (const key in obj) {
+    elem.find(`[data-name="${key}"]`).text(obj[key]);
+  }
+};
+
+learnjs.problemView = (data) => {
+  const problemNumber = parseInt(data, 10);
+  const view = $('.templates .problem-view').clone();
+  const title = `Problem #${problemNumber}`;
+  view.find('.title').text(title);
+  learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
+  return view;
 }
 
 learnjs.showView = (hash) => {
